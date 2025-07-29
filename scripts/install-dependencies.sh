73085cc3b00588a07face68025b3e9c228bc4356
Eric Tai <ericloong90@gmail.com>
2025-07-30 02:25:37 +0800

    Further fixes to permissioning

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -3,12 +3,18 @@ set -e
 
 echo "Installing dependencies..."
 
-cd /home/ec2-user/accad-demo-flask
+DEPLOY_DIR="/home/ec2-user/accad-demo-flask"
 
-if [ ! -d "venv" ]; then
-  python3 -m venv venv
-fi
+# Make sure the deploy user owns the directory (important!)
+sudo chown -R ec2-user:ec2-user $DEPLOY_DIR
 
+# Switch to working directory
+cd $DEPLOY_DIR
+
+# Use ec2-user’s shell to create and activate venv
+sudo -u ec2-user bash <<EOF
+cd $DEPLOY_DIR
+python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
